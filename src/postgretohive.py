d4a9233ccd2b6fcabfbb17b9eb90802e3959146f
Hasnain081992 <Hasnain081992@gmail.com>
2024-12-24 01:11:08 +0000

    ok

--- src/postgretohive.py
+++ src/postgretohive.py
@@ -1,15 +1,20 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.master("local").appName("projectbit").config("spark.driver.memory", "4g").config("spark.executor.memory", "4g").config("spark.executor.cores", "2").config("spark.sql.shuffle.partitions", "200") .enableHiveSupport()enableHiveSupport().getOrCreate()
+# Create SparkSession with Hive support
+spark = SparkSession.builder .master("local") .appName("projectbit") .enableHiveSupport() .getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "bitcoin_2025").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+# Read data from PostgreSQL
+df = spark.read.format("jdbc") .option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver") .option("dbtable", "bitcoin_2025").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+
+# Print schema to verify structure
 df.printSchema()
-#done
-# Add year and month columns for partitioning
-df_with_partition_cols = df.withColumn("year", year(col("Datetime"))) .withColumn("month", month(col("Datetime")))
-# Write data to Hive table with partitioning by year and month
-df_with_partition_cols.repartition("year","month").write .mode("overwrite") .partitionBy("year", "month") .saveAsTable("project2024.hasan_bitcoin")
 
-#df.write.mode("overwrite").saveAsTable("project2024.hasan_bitcoin")
-print("Successfully Load to Hive")
+# Partition the data based on a column (e.g., "year")
+partitioned_column = "year"  # Replace this with a relevant column from your dataset
+df_with_partition = df.withColumn(partitioned_column, year(col("timestamp")))  # Example partition logic
+
+# Write data to Hive table with partitioning
+df_with_partition.write.mode("overwrite").partitionBy(partitioned_column).saveAsTable("project2024.hasan_bitcoin")
+
+print("Successfully loaded to Hive with partitioning")
