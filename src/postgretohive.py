eecd42a2ec93896892e1b82fa389f38dd42d8eaf
Hasnain081992 <Hasnain081992@gmail.com>
2024-12-24 01:15:12 +0000

    ok

--- src/postgretohive.py
+++ src/postgretohive.py
@@ -10,11 +10,7 @@ df = spark.read.format("jdbc") .option("url", "jdbc:postgresql://18.132.73.146:5
 # Print schema to verify structure
 df.printSchema()
 
-# Partition the data based on a column (e.g., "year")
-partitioned_column = "year"  # Replace this with a relevant column from your dataset
-df_with_partition = df.withColumn(partitioned_column, year(col("timestamp")))  # Example partition logic
+df.write.mode("overwrite").saveAsTable("project2024.hasancoin")
+print("Successfully Load to Hive")
 
-# Write data to Hive table with partitioning
-df_with_partition.write.mode("overwrite").partitionBy(partitioned_column).saveAsTable("project2024.hasan_bitcoin")
 
-print("Successfully loaded to Hive with partitioning")
